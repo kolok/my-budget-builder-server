29b0464ff9913b6ad3cf3326eac775ee32e040c4
Nicolas Oudard <noudard@weborama.com>
2019-06-14 12:29:51 +0200

    remove menu from store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,10 +4,9 @@ import Vuex from 'vuex'
 /** Modules **/
 import auth from './modules/auth'
 import things from './modules/things'
-import menu from './modules/menu'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: { auth, things, menu }
+  modules: { auth, things }
 })
