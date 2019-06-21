49d3f5bdd646de58e794cc382a71f26bb40c30db
Nicolas Oudard <noudard@weborama.com>
2019-06-21 15:35:40 +0200

    remove Layout from Common.vue

--- client/src/views/Common.vue
+++ client/src/views/Common.vue
@@ -1,26 +1,29 @@
 <template>
-  <Layout style="height:100%;">
-    <Sider
-      v-model="isCollapsed"
-      :style="{background: '#fff'}"
-      :collapsed-width="78"
-      collapsible
-    >
-      <app-sider :is-collapsed="isCollapsed" />
-    </Sider>
-    <Layout style="padding:24px">
-        <router-view />
-    </Layout>
-  </Layout>
+  <el-container>
+    <el-aside width="200px">
+      <Sider
+        v-model="isCollapsed"
+        :style="{background: '#fff'}"
+        :collapsed-width="78"
+        collapsible
+      >
+        <app-sider :is-collapsed="isCollapsed" />
+      </Sider>
+    </el-aside>
+    <el-main style="background: #f5f7f9;height:100vh">
+      <router-view />
+    </el-main>
+  </el-container>
 </template>
 
+
 <script>
-import { Layout, Sider, Content, Breadcrumb, BreadcrumbItem } from 'iview'
+import { Sider } from 'iview'
 
 export default {
   components: {
     'app-sider': () => import('../components/Sider.vue'),
-    Layout, Sider, Content, Breadcrumb, BreadcrumbItem
+    Sider
   },
   data() {
     return {
