a1faa126e0cf31cb10512dbd0e96848fbf4dca80
Nicolas Oudard <noudard@weborama.com>
2019-07-08 09:02:23 +0200

    remove iview Layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,20 +2,17 @@
   <div class="layout">
     <el-container style="min-height: 100vh">
       <el-header style="height: 64px;padding:0;"><app-header /></el-header>
-      <el-main style="height: 100vh;padding:0;"><router-view /></el-main>
+      <el-main style="height: 100vh;padding:10px;"><router-view /></el-main>
     </el-container>
   </div>
 </template>
 
 <script>
   import Header from './components/Header.vue'
-  import { Layout } from 'iview'
 
   export default {
     components: {
       'app-header': Header,
-      // 'app-footer': Footer,
-      Layout
     },
     data() {
       return {
