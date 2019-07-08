5574b96ed7d062d4da7ecbb1d8f2d6979d404dc3
Nicolas Oudard <noudard@weborama.com>
2019-07-08 11:19:52 +0200

    remove the slider actions from the header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -6,43 +6,21 @@
     <el-col :span="20">
       <div class="layout-nav" v-if="isAuthenticated">
         <el-menu
-          :default-active="defaultMenu"
+          default-active="teamDashboard"
           class="header-menu"
           mode="horizontal"
           @select="handleClickHeader"
           background-color="#545c64"
           text-color="#fff"
-          active-text-color="#ffd04b"
-        >
-          <el-submenu
-            v-for="item in menuItems"
-            :key="item.index"
-            :index="item.index"
-          >
-            <template slot="title">
-              <i :class="item.icon"/>
-              {{item.name}}
-            </template>
-            <el-menu-item
-              v-for="subitem in item.children"
-              :key="subitem.index"
-              :index="subitem.index"
-            >
-              <i :class="subitem.icon"/>
-              {{subitem.name}}
-            </el-menu-item>
-          </el-submenu>
-
+          active-text-color="#ffd04b">
+          <el-menu-item index="teamDashboard"><i class="el-icon-user"/>Team</el-menu-item>
+          <el-menu-item index="planning"><i class="el-icon-c-scale-to-original"/>Planning</el-menu-item>
+          <el-menu-item index="execution"><i class="el-icon-folder-checked"/>Execution</el-menu-item>
+          <el-menu-item index="company"><i class="el-icon-setting"/>Settings</el-menu-item>
           <el-submenu index="adminsettings" style="float:right">
             <template slot="title">{{ getCurrentUser.name }}</template>
-            <el-menu-item
-              v-for="adminitem in adminMenuItems"
-              :key="adminitem.index"
-              :index="adminitem.index"
-            >
-              <i :class="adminitem.icon"/>
-              {{adminitem.name}}
-            </el-menu-item>
+            <el-menu-item index="account">Account</el-menu-item>
+            <el-menu-item index="logout">Logout</el-menu-item>
           </el-submenu>
         </el-menu>
       </div>
@@ -58,144 +36,12 @@
     },
     data() {
       return {
-        adminMenuItems: [
-          {
-            index: 'profile',
-            name: 'Your profile',
-            path: '/profile',
-            icon: 'el-icon-user'
-          },
-          {
-            index: 'credentials',
-            name: 'Credentials',
-            path: '/credentials',
-            icon: 'el-icon-user'
-          },
-          {
-            index: 'account',
-            path: '/account',
-            name: 'Account',
-            icon: 'el-icon-house',
-          },
-          {
-            index: 'users',
-            path: '/users',
-            name: 'Users',
-            icon: 'el-icon-user',
-            roles: ['admin']
-            },
-          {
-            index: 'things',
-            name: 'Things',
-            path: '/things',
-            icon: 'el-icon-user'
-            },
-          {
-            index: 'logout',
-            name: 'Logout',
-            path: '/logout',
-            icon: 'el-icon-logout'
-          }
-        ],
-        menuItems: [
-          {
-            index: 'teamDashboard',
-            name: 'Team',
-            path: '/teamDashboard',
-            icon: 'el-icon-user',
-            children : [
-              {
-                index: 'teamDashboard',
-                name: 'Dashboard',
-                path: '/teamDashboard',
-                icon: 'el-icon-pie-chart'
-              },
-              {
-                index: 'teams',
-                name: 'Teams',
-                path: '/teams',
-                icon: 'el-icon-user'
-              },
-              {
-                index: 'positions',
-                path: '/positions',
-                name: 'Positions',
-                icon: 'el-icon-refresh-right'
-              },
-              {
-                index: 'employees',
-                name: 'Employees',
-                path: '/employees',
-                icon: 'el-icon-user'
-              }
-            ]
-          },
-          {
-            index: 'planning',
-            name: 'Planning',
-            path: '/planning',
-            icon: 'el-icon-c-scale-to-original',
-            children : [
-              {
-                index: 'planning',
-                name: 'Planning',
-                path: '/planning',
-                icon: 'el-icon-c-scale-to-original'
-              }
-            ]
-          },
-          {
-            index: 'execution',
-            name: 'Execution',
-            path: '/execution',
-            icon: 'el-icon-folder-checked',
-            children : [
-              {
-                index: 'execution',
-                name: 'Execution',
-                path: '/execution',
-                icon: 'el-icon-folder-checked'
-              }
-            ]
-          },
-          {
-            index: 'company',
-            name: 'Settings',
-            path: '/company',
-            icon: 'el-icon-setting',
-            children : [
-              {
-                index: 'company',
-                name: 'Settings',
-                path: '/company',
-                icon: 'el-icon-house'
-              },
-              {
-                index: 'entities',
-                name: 'Entities',
-                path: '/entities',
-                icon: 'el-icon-files'
-              },
-              {
-                index: 'teams',
-                name: 'Teams',
-                path: '/teams',
-                icon: 'el-icon-user'
-              }
-            ]
-          }
-        ],
-        defaultMenu : ''
       }
     },
     computed: {
     ...mapGetters(['isAuthenticated', 'getCurrentUser'])
     },
-    beforeMount(){
-      this.getDefaultMenu()
-    },
     methods: {
-      ...mapMutations(['UpdateMenuItemsBy']),
       logout: function() {
         this.$store.dispatch('logout')
         this.$router.push('/login')
@@ -204,12 +50,8 @@
         if (name === 'logout') {
           this.logout()
         } else {
-          this.UpdateMenuItemsBy(name)
           this.$router.push(name)
         }
-      },
-      getDefaultMenu: function () {
-        this.defaultMenu = this.menuItems[0].index
       }
     }
   }
